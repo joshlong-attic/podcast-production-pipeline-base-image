330d0fd6b910e49ee0a575f64ca566fb35ec81be
Josh Long <josh@joshlong.com>
2019-05-07 01:33:45 -0300

    travis.yml

--- build_docker.sh
+++ build_docker.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
+
 set -e
 app=python-podcast-processor-base-image
 docker build -t $app .
-# docker tag $app starbuxman/$app
-# docker push starbuxman/$app
+docker tag $app starbuxman/$app
+docker push starbuxman/$app
